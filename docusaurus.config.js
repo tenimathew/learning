29f246a80c833655842b77d9196506bf2a1637a8
tenimathew <tenimathew@gmail.com>
2025-03-27 16:19:29 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "My Learning",
   tagline: "Learn something new every day",
-  url: "https://tenimathew.github.io/learning",
+  url: "https://tenimathew.github.io/learning/",
   baseUrl: "/",
   trailingSlash: false,
   onBrokenLinks: "warn", //"throw",
